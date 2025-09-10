e95772946b56cdefdfc8f0379d7d9d599cb9cb0c
SriVignesh-Space <srivignesh1149@gmail.com>
2025-09-10 14:54:02 +0530

    Basic Structure added

---
+++ Volume.py
@@ -0,0 +1,18 @@
+class Volume:
+    def Sphere(self):
+        pass
+
+    def Cylinder(self):
+        pass
+
+    def Cone(self):
+        pass
+
+    def Cube(self):
+        pass
+
+    def Cuboid(self):
+        pass
+
+    def Hemisphere(self):
+        pass
