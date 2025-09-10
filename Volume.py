8d8cde75842376c28f1e55f9ea0dc82fa1376d89
Hariveeraprasad-007 <arikatlahariveeraprasad007@gmail.com>
2025-09-10 15:56:39 +0530

    code written for cube,cuboid and hemisphere

--- Volume.py
+++ Volume.py
@@ -8,11 +8,11 @@ class Volume:
     def Cone(self):
         pass
 
-    def Cube(self):
-        pass
+    def Cube(self,a):
+        return a**3
 
-    def Cuboid(self):
-        pass
-
-    def Hemisphere(self):
-        pass
+    def Cuboid(self,length,breadth,height):
+        return length*breadth*height
+    
+    def Hemisphere(self,radius):
+        return (2/3)*(22/7)*(radius**3)
