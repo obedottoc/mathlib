4f97c44adf84dbdce46ef260a81eeede1ebae185
karthi-s-13 <karthikeyan13ind@gmail.com>
2025-09-10 20:59:48 +0530

    Success

--- Statistics.py
+++ Statistics.py
@@ -23,3 +23,29 @@ class Statistics:
             return sum(squared_diffs) / (len(data) - 1)
         else:
             return sum(squared_diffs) / len(data)
+    
+    def z_test(sample_mean, population_mean, standard_deviation, sample_size):
+        """
+        One-sample Z-test statistic
+        """
+        return (sample_mean - population_mean) / (standard_deviation / math.sqrt(sample_size))
+
+    def t_test(sample_mean, population_mean, sample_standard_deviation, sample_size):
+        """
+        One-sample T-test statistic
+        """
+        return (sample_mean - population_mean) / (sample_standard_deviation / math.sqrt(sample_size))
+
+    def chi_square_test(observed, expected):
+        """
+        Chi-square statistic
+        """
+        return (observed - expected) ** 2 / expected
+
+    def f_test(variance1, variance2):
+        """
+        F-test statistic
+        """
+        return variance1 / variance2
+
+
