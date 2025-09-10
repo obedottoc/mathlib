1f4e5ad5eb353e27e4e0a0f6470170f7d0574019
Kabe-Innovates <kabe.innovates@gmail.com>
2025-09-10 17:30:14 +0530

    Update : Conversions.py - Temperature Class added

---
+++ Conversions.py
@@ -0,0 +1,21 @@
+class Temperature:
+    @staticmethod
+    def celsius_to_fahrenheit(celsius: float) -> float:
+        """Convert Celsius to Fahrenheit."""
+        return (celsius * 9/5) + 32
+
+    @staticmethod
+    def fahrenheit_to_celsius(fahrenheit: float) -> float:
+        """Convert Fahrenheit to Celsius."""
+        return (fahrenheit - 32) * 5/9
+
+    @staticmethod
+    def celsius_to_kelvin(celsius: float) -> float:
+        """Convert Celsius to Kelvin."""
+        return celsius + 273.15
+
+    @staticmethod
+    def kelvin_to_celsius(kelvin: float) -> float:
+        """Convert Kelvin to Celsius."""
+        return kelvin - 273.15
+
